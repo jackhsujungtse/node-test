de5d5df51106a7dbfe243d944e9e0d2577aa9018
jackhsujungtse <jackhsujungtse@gmail.com>
2017-04-06 16:24:18 +0800

    testing

--- app.js
+++ app.js
@@ -1,18 +1,25 @@
 var express = require('express');
 var app = express();
-var bodyParser = require('body-parser');
+
 var port = process.env.PORT || 3000;
-var urlencodedParser = bodyParser.urlencoded({ extended: false});
 
-app.use(express.static(__dirname + '/public'));
-app.set('view engine', 'ejs');
+app.use('/assets', express.static(__dirname + '/public'));
 
-app.get('/', function(req, res) {
-	res.render('index');
+app.use('/', function (req, res, next) {
+	console.log('Request Url:' + req.url);
+	next();
 });
 
-// app.get('/index.html', function(req, res) {
-// 	res.render('index');
-// });
+app.get('/', function(req, res) {
+	res.send('<html><head><link href=assets/style.css type=text/css rel=stylesheet /></head><body><h1>Hello world!</h1></body></html>');
+});
+
+app.get('/person/:id', function(req, res) {
+	res.send('<html><head></head><body><h1>Person: ' + req.params.id + '</h1></body></html>');
+});
+
+app.get('/api', function(req, res) {
+	res.json({ firstname: 'John', lastname: 'Doe' });
+});
 
 app.listen(port);
