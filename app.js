ed06f6a5a873445ec7d3f996bf2a31f4039cd60f
jackhsujungtse <jackhsujungtse@gmail.com>
2017-04-06 16:16:00 +0800

    node-test for mywebsite

--- app.js
+++ app.js
@@ -1,25 +1,18 @@
 var express = require('express');
 var app = express();
-
+var bodyParser = require('body-parser');
 var port = process.env.PORT || 3000;
+var urlencodedParser = bodyParser.urlencoded({ extended: false});
 
-app.use('/assets', express.static(__dirname + '/public'));
-
-app.use('/', function (req, res, next) {
-	console.log('Request Url:' + req.url);
-	next();
-});
+app.use(express.static(__dirname + '/public'));
+app.set('view engine', 'ejs');
 
 app.get('/', function(req, res) {
-	res.send('<html><head><link href=assets/style.css type=text/css rel=stylesheet /></head><body><h1>Hello world!</h1></body></html>');
+	res.render('index');
 });
 
-app.get('/person/:id', function(req, res) {
-	res.send('<html><head></head><body><h1>Person: ' + req.params.id + '</h1></body></html>');
-});
-
-app.get('/api', function(req, res) {
-	res.json({ firstname: 'John', lastname: 'Doe' });
-});
+// app.get('/index.html', function(req, res) {
+// 	res.render('index');
+// });
 
 app.listen(port);
